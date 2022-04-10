e6822674b2073ab3be11495ae3f472b7d5fe4c48
Franzi Graml <franzi.graml@gmx.de>
2022-04-10 22:45:09 +0200

    Add testing.js final

--- franzi/testing.js
+++ franzi/testing.js
@@ -5,7 +5,7 @@ export async function fetchApi(apiUrl) {
     const response = await fetch(apiUrl);
     const data = await response.json();
     const output = data.results;
-    filterCards(output);
+    return output;
   } catch (error) {
     console.error(`Upps das war ein Fehler: ${error}`);
     const output = null;
@@ -15,38 +15,54 @@ export async function fetchApi(apiUrl) {
 
 const dataButton = document.querySelector("button");
 const filterForm = document.querySelector("[data-js=filter-form]");
+const container = document.querySelector("[data-js=cardsContainer]");
+let filter = "all";
 
 filterForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  fetchApi(url);
+  event.preventDefault(); // Damit Formular nicht bei jedem Click abgeschickt wird
+  fetchApi(url).then((result) => renderRickMorty(result));
+  // fetchApi-function wird aufgerufen --> daten kommen als promise --> .then löst promise auf >> funktion renderRickMorty wird mit promise-Daten aufgerufen
   filterForm.addEventListener("change", () => {
-    let Filter = filterForm.elements["tag-filter"].value;
+    filter = filterForm.elements["tag-filter"].value;
   });
 });
 
+// die gespeicherten Daten von renderRickMorty speichere ich im Cards-Parameter
 function renderRickMorty(cards) {
-  const unList = document.createElement("ul");
-  document.body.append(unList);
-
-  cards.forEach((data) => {
-    const listElement = document.createElement("li");
-    const image = document.createElement("img");
-    image.src = data.image;
-    listElement.textContent = data.name;
-    unList.appendChild(image);
-    unList.appendChild(listElement);
+  container.innerHTML = ""; // ich leere meinen Container
+  let filtered = []; // leeres Array initialisieren
+  //
+  filtered = cards.filter((card) => card.status === filter || filter === "all");
+  filtered.forEach((card) => {
+    // jede Karte wird gefiltert
+    const listElement = document.createElement("li"); // neues li-element erzeugen
+    const image = document.createElement("img"); // neues img-element erzeugen
+    image.src = card.image; // img-element benennen
+    image.classList.add(
+      // img-element eine klasse vergeben => in einer if--else function
+      card.status === "Alive" //wenn status 'Alive' dann 'grün'
+        ? "borderGreen"
+        : card.status === "Dead" // wenn status 'Dead' dann 'rot'
+        ? "borderRed"
+        : "borderBlue" // wenn was 'anderes' dann 'blau'
+    );
+    listElement.textContent = card.name; // li-element benennen
+    container.appendChild(image);
+    container.appendChild(listElement); // li/img-element zu Conainer hinzufügen
   });
 }
-
-function filterCards(cards) {
-  let currentFilter = "all";
-  filterForm.addEventListener("change", () => {
-    let Filter = filterForm.elements["tag-filter"].value;
-    currentFilter = Filter;
-  });
-  let filteredCards = cards.filter(
-    (card) => card.status === currentFilter || currentFilter === "all"
-  );
-  renderRickMorty(filteredCards);
-  console.log(currentFilter);
+/* 
+getCardStatus(card)
+function getCardStatus(card) {
+  let status;
+  if (card.status === "Alive") {
+    status = "borderGreen";
+  }
+  if (card.status === "Dead") {
+    status = "borderRed";
+  } else {
+    status = "borderBlue";
+  }
+  return status;
 }
+ */
